3827dc348236132e676573647cf16fbf98626e22
Emily Bowen <emilywbowen@gmail.com>
2022-10-31 15:06:46 -0500

    failed olympics

---
+++ dom-Olympics/failed olympics/index.js
@@ -0,0 +1,17 @@
+var h1 = document.createElement("h1")
+
+h1.textContent = "JavaScript made this!"
+
+var header = document.getElementById("header");
+h1.textContent = "JavaScript made this!";
+header.append(h1)
+
+//h1.className = "header";
+//document.getElementById("header").appendChild(h1);
+//document.body.textContent="JavaScript made this!";
+
+
+// const subTitle = document.createElement("subTitle");
+// subTitle.innerHTML = "<span class .name=>Emily</span> wrote the JavaScript";
+// document.getElementById("subTitle").appendChild(subTitle);
+
