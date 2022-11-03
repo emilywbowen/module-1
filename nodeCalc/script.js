c772b4a1d894454f41e165ee33c86804a620d1e8
Emily Bowen <emilywbowen@gmail.com>
2022-11-03 12:26:53 -0500

    it's working!

--- nodeCalc/script.js
+++ nodeCalc/script.js
@@ -1,12 +1,66 @@
+// //const readline = require("readline-sync")
+// var readlineSync = require('readline-sync');
+// var userName = readlineSync.question("Who are you? ");
+// console.log("Hey " + userName +"!");
+
+// const firstNumber = readlineSync.question("What is your first number? ");
+// console.log(firstNumber);
+
+// const secondNumber = readlineSync.question("What is your second number? ");
+// console.log(secondNumber);
+
+// const operations = ["add", "sub", "mul", "div"];
+
+// const selectOperation = readlineSync.keyInSelect(operations, "Do you want to add, sub, mul, or div? ");
+
+// function addNum(firstNumber, secondNumber) {
+//     return firstNumber + secondNumber
+// }
+// function subNum(firstNumber, secondNumber) {
+//     return firstNumber - secondNumber
+// }
+// function mulNum(firstNumber, secondNumber) {
+//     return firstNumber * secondNumber
+// }
+// function divNum(firstNumber, secondNumber) {
+//     return firstNumber / secondNumber
+// }
+
+// if(selectOperation === 0) {
+//     addNum(firstNumber, secondNumber);
+// }else if(selectOperation === 1) {
+//     subNum(firstNumber, secondNumber);
+// }else if(selectOperation === 2) {
+//     mulNum(firstNumber, secondNumber);
+// }else if(selectOperation === 3){
+//     divNum(firstNumber, secondNumber);
+// }else{
+//     console.log("ABORT MISSION!");
+// }
+// // if(key === "a") {
+//     console.log(firstNumber + secondNumber);
+//     }
+// else if (key === "s") {
+//     console.log(firstNumber - secondNumber);
+//     }
+    
+//     else if (key === "m") {
+//     console.log(firstNumber * secondNumber);
+// }
+//     else if (key === "d") {
+//     console.log(firstNumber / secondNumber);
+// }
+//console.log("You chose " + operation +"! Please wait!");
+
 //const readline = require("readline-sync")
 var readlineSync = require('readline-sync');
 var userName = readlineSync.question("Who are you? ");
-console.log("Hey " + userName +"!");
+console.log("Hey " + userName +"! Let's do some math!");
 
-const firstNumber = readlineSync.question("What is your first number? ");
+const firstNumber = readlineSync.questionFloat("What is your first number? ");
 console.log(firstNumber);
 
-const secondNumber = readlineSync.question("What is your second number? ");
+const secondNumber = readlineSync.questionFloat("What is your second number? ");
 console.log(secondNumber);
 
 const operations = ["add", "sub", "mul", "div"];
@@ -14,16 +68,16 @@ const operations = ["add", "sub", "mul", "div"];
 const selectOperation = readlineSync.keyInSelect(operations, "Do you want to add, sub, mul, or div? ");
 
 function addNum(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
+    console.log("The answer is : " + (firstNumber + secondNumber));
 }
 function subNum(firstNumber, secondNumber) {
-    return firstNumber - secondNumber
+    console.log("The answer is : " + (firstNumber - secondNumber));
 }
 function mulNum(firstNumber, secondNumber) {
-    return firstNumber * secondNumber
+    console.log("The answer is : " + (firstNumber * secondNumber));
 }
 function divNum(firstNumber, secondNumber) {
-    return firstNumber / secondNumber
+    console.log("The answer is : " + (firstNumber / secondNumber));
 }
 
 if(selectOperation === 0) {
@@ -32,6 +86,7 @@ if(selectOperation === 0) {
     subNum(firstNumber, secondNumber);
 }else if(selectOperation === 2) {
     mulNum(firstNumber, secondNumber);
+
 }else if(selectOperation === 3){
     divNum(firstNumber, secondNumber);
 }else{
@@ -43,7 +98,7 @@ if(selectOperation === 0) {
 // else if (key === "s") {
 //     console.log(firstNumber - secondNumber);
 //     }
-    
+
 //     else if (key === "m") {
 //     console.log(firstNumber * secondNumber);
 // }
