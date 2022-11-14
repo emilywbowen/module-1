2ae612d657b7e74762f2626020a06fdc1a28876d
Emily Bowen <emilywbowen@gmail.com>
2022-11-14 14:38:50 -0600

    counting computers correctly now

--- loopsAndArrays/script.js
+++ loopsAndArrays/script.js
@@ -1,7 +1,38 @@
+//how many computers are there?
+
 var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"];
 var numOfComputer = 0
 for(var i = 0; i < officeItems.length; i++) {
     if(officeItems[i] === "computer")
-    i++;
-    console.log(numOfComputer)
+    numOfComputer++;
+
 }
+console.log(numOfComputer)
+
+
+//Old enough exercise:
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+  
